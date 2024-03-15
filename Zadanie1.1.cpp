1fb04665ccf683c739de0ee97ec9363352bec6ca
Олександр Казаков <Oleksandr.Kazakov@infiz.khpi.edu.ua>
2024-03-15 08:56:54 +0200

    Добавьте файлы проекта.

---
+++ Zadanie1.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    
+    int array[] = { 1, 15, 10, 228, 302 };
+
+    int size = sizeof(array) / sizeof(array[0]);
+
+    int sum = 0;
+
+    for (int i = 0; i < size; ++i) {
+        sum += array[i];
+    }
+
+    cout << "The sum of array elements: " << sum << endl;
+
+    return 0;
+}
