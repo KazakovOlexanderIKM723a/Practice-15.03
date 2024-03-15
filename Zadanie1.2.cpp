a2b27c45308d41d33ac4377901eb99da74bd9584
KazakovOlexanderIKM723a <163044623+KazakovOlexanderIKM723a@users.noreply.github.com>
2024-03-15 09:06:14 +0200

    Add files via upload

---
+++ Zadanie1.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    int array[] = { 5, 3, 7, 1, 9, 2 };
+
+    int size = sizeof(array) / sizeof(array[0]);
+
+    int minElement = array[0];
+    int maxElement = array[0];
+
+    for (int i = 1; i < size; ++i) {
+        if (array[i] < minElement) {
+            minElement = array[i];
+        }
+        if (array[i] > maxElement) {
+            maxElement = array[i];
+        }
+    }
+
+    cout << "Min element " << minElement << endl;
+    cout << "Max element " << maxElement << endl;
+
+    return 0;
+}
