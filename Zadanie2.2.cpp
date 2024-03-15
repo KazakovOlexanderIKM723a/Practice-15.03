cf9373e1581fa5871f5007f5076925094c714696
KazakovOlexanderIKM723a <163044623+KazakovOlexanderIKM723a@users.noreply.github.com>
2024-03-15 09:40:21 +0200

    Add files via upload

---
+++ Zadanie2.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    const int size = 3; 
+    int matrix[size][size] = { {13, 2, 21},
+                               {99, 61, 57},
+                               {71, 8, 322} };
+
+    int mainDiagonalSum = 0;
+    int secondaryDiagonalSum = 0;
+
+    for (int i = 0; i < size; ++i) {
+        mainDiagonalSum += matrix[i][i];
+    }
+
+    for (int i = 0; i < size; ++i) {
+        secondaryDiagonalSum += matrix[i][size - 1 - i];
+    }
+
+    cout << "The sum of elements on main diagonal: " << mainDiagonalSum << endl;
+    cout << "The sum of elements on side diagonal: " << secondaryDiagonalSum << endl;
+
+    return 0;
+}
---
+++ Zadanie2.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    const int rows = 3; 
+    const int cols = 3; 
+    int matrix[rows][cols] = { {1, 2, 3},
+                               {4, 5, 6},
+                               {7, 8, 9} }; 
+
+    int lowerBound = 3; 
+    int upperBound = 7; 
+
+    
+    cout << "We are looking for elements in the range [" << lowerBound << ", " << upperBound << "]" << endl;
+
+    
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (matrix[i][j] >= lowerBound && matrix[i][j] <= upperBound) {
+                cout << "Element " << matrix[i][j] << " is in the given range at the position [" << i << "][" << j << "]" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
